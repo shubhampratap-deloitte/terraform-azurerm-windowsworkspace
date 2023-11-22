f43dcb49f4e33c1db33134e478883245feb659dd
shubhampratap-deloitte <shupundir@deloitte.com>
2023-11-22 14:26:14 +0530

    added vm window

---
+++ main.tf
@@ -0,0 +1,36 @@
+//--------------------------------------------------------------------
+// Variables
+variable "vmwindow_admin_password" {}
+variable "vmwindow_client_id" {}
+variable "vmwindow_client_secret" {}
+variable "vmwindow_image_os" {}
+variable "vmwindow_location" {}
+variable "vmwindow_name" {}
+variable "vmwindow_os_disk" {}
+variable "vmwindow_os_simple" {}
+variable "vmwindow_resource_group_name" {}
+variable "vmwindow_size" {}
+variable "vmwindow_subnet_id" {}
+variable "vmwindow_subscription_id" {}
+variable "vmwindow_tenant_id" {}
+
+//--------------------------------------------------------------------
+// Modules
+module "vmwindow" {
+  source  = "app.terraform.io/edj_demo_org_000/vmwindow/azurerm"
+  version = "1.0.6"
+
+  admin_password = "${var.vmwindow_admin_password}"
+  client_id = "${var.vmwindow_client_id}"
+  client_secret = "${var.vmwindow_client_secret}"
+  image_os = "${var.vmwindow_image_os}"
+  location = "${var.vmwindow_location}"
+  name = "${var.vmwindow_name}"
+  os_disk = "${var.vmwindow_os_disk}"
+  os_simple = "${var.vmwindow_os_simple}"
+  resource_group_name = "${var.vmwindow_resource_group_name}"
+  size = "${var.vmwindow_size}"
+  subnet_id = "${var.vmwindow_subnet_id}"
+  subscription_id = "${var.vmwindow_subscription_id}"
+  tenant_id = "${var.vmwindow_tenant_id}"
+}
