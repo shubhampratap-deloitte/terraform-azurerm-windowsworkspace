89497eb348cd497dbc13aadb91110f28cf990f44
shubhampratap-deloitte <shupundir@deloitte.com>
2023-12-05 19:38:53 +0530

    added new 26 version

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ variable "vmwindow_vm_count" {}
 // Modules
 module "vmwindow" {
   source  = "app.terraform.io/edj_demo_org_000/vmwindow/azurerm"
-  version = "1.0.25"
+  version = "1.0.26"
 
   admin_password = "${var.vmwindow_admin_password}"
   admin_username = "${var.vmwindow_admin_username}"
